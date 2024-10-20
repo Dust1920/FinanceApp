30ad65e444d93581b6189b22fabdb6be278fdf9e
Dust1920 <a218208885@gmail.com>
2024-10-19 23:20:52 -0700

    plot cut, payment and I-FM

--- finances/credit_cards.py
+++ finances/credit_cards.py
@@ -8,7 +8,7 @@
 import pandas as pd
 import plotly.express as px
 
-
+# Read Data (Credit Cards)
 raw = pd.read_excel("data\\CreditCards.xlsx", sheet_name=None)
 
 # General Info of Credit Cards.
@@ -19,14 +19,55 @@ raw_g = raw_g.set_index("Name", drop=True)
 TOTAL_CREDIT = raw_g['Credit Limit'].sum()
 
 # Auxiliar Functions
-def list_cc(df):
+def list_cc():
     """
         Get a list of Credit Cards written in Finance App
     """
     hist = "History "
-    raw_sheets = list(df.keys())
+    raw_sheets = list(raw.keys())
     credit_cards = []
     for k in raw_sheets:
         if k.startswith(hist):
             credit_cards.append(k)
     return credit_cards
+
+def all_cc_df(col, **kwargs):
+    """
+    Generate a Cut Off Line Plot
+    """
+    cards = kwargs.get("cards","*")
+    if cards == "*":
+        cards = list_cc()
+    name_cards = [x.split(" ")[1] for x in cards]
+    cols = ["Cut","Payment","I-FM"]
+    if not col in cols:
+        return -1
+    col_df = pd.DataFrame()
+    for nc, cc in enumerate(cards):
+        df = raw[cc]
+        df['Date'] = df['Month'].transform(lambda x: str(x)[:3])
+        df['Date'] += df['Year'].transform(lambda x: str(x)[2:])
+        df.set_index('Date', inplace=True, drop=True)
+        if nc == 0:
+            col_df = pd.DataFrame(index = df.index, columns=name_cards)
+        col_df.loc[:, name_cards[nc]] = df.loc[:,'Cut']
+    return col_df
+
+def df_to_plotly_df(df: pd.DataFrame, col):
+    """
+    Convert a Pandas DataFrame a DataFrame to Plot with Plotly. 
+    """
+    nx = len(df.index)
+    ny = len(df.columns)
+    result = pd.DataFrame(index = list(range(nx * ny)), columns=["Date","Card",col])
+    i = 0
+    for x in df.index:
+        for y in df.columns:
+            result.loc[i, ["Date","Card",col]] = [x,y,df.loc[x,y]]
+            i = i + 1
+    return result
+
+df_cut = all_cc_df("Cut")
+plot_cut = df_to_plotly_df(df_cut, "Cut")
+fig_cut = px.line(plot_cut, x = "Date", y = "Cut", color = "Card")
+fig_cut.show()
