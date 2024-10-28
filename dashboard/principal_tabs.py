db390a509817a2b089f14e70a5b5f34707055144
Dust1920 <a218208885@gmail.com>
2024-10-28 12:42:50 -0700

    tabs

--- dashboard/principal_tabs.py
+++ dashboard/principal_tabs.py
@@ -1,40 +1,62 @@
 from dash import html, dcc
 
+MULTI_COLUMN = {"padding": "5px",
+                "display": "flex",
+                "background-color":"red"}
+
 # Portfoly
-
-
 portfoly = html.Div(
     children=[
         html.H1("Portafolio de Inversiones"),
-    ]
-)
+        html.Div(children=[
+            # Distribución de Portafolio
+        ]),
+        html.Div(children=[
+            # Distribución de Activos.
+            html.H1("Registro", style={"text-align":"center"})
+        ])
+        ])
 
 # Summary
-
 sum_ary = html.Div(children=[
     html.H1("Resumen general"),
+    html.Div(children=[
+        html.Div(children=[
+            # Dias para la fecha de corte.
+            html.H1("Dias para pagar la tarjeta")
+        ], style={"width":"50%"}),
+        html.Div(children=[
+            # Fecha Actual
+            html.H1("Fecha de Hoy"),
+            html.H2("")
+        ],style = {"width":"50%"}),
+    ], style=MULTI_COLUMN),
+    html.Div(children=[
+        html.Div(children=[
+            # Uso de Crédito Historico por Año
+            html.H1("Crédito Histórico")
+        ], style={"width":"50%"}),
+        html.Div(children=[
+            # Uso de Crédito Mensual
+            html.H1("Uso de Crédito: "),
+            html.H1(""),
+            html.H1("Crédito Total: ")
+        ], style={"width":"50%"})
+    ], style=MULTI_COLUMN)
 ])
 
 
-"""
-    padding: 5px;
-    display: flex;
-    align-items: start;
-"""
-
-
 # Expenses
-
 expenses = html.Div(children=[
     html.H1("Gastos Personales", style = {"text-align": "center"}),
     html.Div(children=[
         html.Div(children=[
-            html.H1("Consumo Mensual")
+            html.H1("Consumo Mensual"),
+            # Grafica de Consumo Total por Mes
         ], style= {"width": "50%"}),
         html.Div(children=[
             html.H1("Distribución por Tarjeta")
+            # Grafica por Consumo Total por Mes
         ], style= {"width": "50%"})
-    ], style={"padding": "5px",
-              "display": "flex"
-              })
+    ], style=MULTI_COLUMN)
 ])
